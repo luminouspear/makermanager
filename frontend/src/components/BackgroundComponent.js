cf055cd22c06ff7cf62a2a4bccf79709f2ae8014
luminouspear <luminouspears@gmail.com>
2023-03-16 11:04:31 -0400

    Refactored components

--- frontend/src/components/BackgroundComponent.js
+++ frontend/src/components/BackgroundComponent.js
@@ -1,6 +1,23 @@
 import React from 'react'
 
 function BackgroundComponent() {
+
+	const backgroundInfo = [
+		{
+			title: "I'm based in",
+			subtitle: "Barrie, Ontario right by the lake",
+		},
+		{
+			title: "I worked at",
+			subtitle: `Lynk Law, Inc
+							Tumbleweed Day Camp`,
+		},
+		{
+			title: "My Roles Were",
+			subtitle: "Client Manager; Assistant Director",
+		},
+	];
+
   return (
 		<section className="container max-w-7xl mx-auto bg-black w-full">
 			<div className="w-full mx-auto text-center">
@@ -9,33 +26,20 @@ function BackgroundComponent() {
 				</h2>
 			</div>
 			<div className="flex flex-col lg:grid lg:grid-flow-col lg:grid-cols-9 lg:gap-8 lg:px-12 md:w-7/12 md:mx-auto space-y-4 md:space-y-8 lg:space-y-0 lg:w-full">
-				<div className="mx-12 lg:mx-0 border-[1px] border-mmpurple p-8 flex justify-center lg:col-span-3">
-					<p className="text-white font-oswald text-4xl uppercase text-center px-8 py-8">
-						<span className="block font-robotoslab text-mmpink text-xl mb-8 font-extrabold">
-							I'm based in
-						</span>{" "}
-						Barrie, Ontario right by the lake
-					</p>
-				</div>
-				<div className="mx-12 lg:mx-0 border-[1px] border-mmpurple p-8 flex justify-center lg:col-span-3">
-					<p className="text-white font-oswald text-4xl uppercase text-center px-8 py-8">
-						<span className="block font-robotoslab text-mmpink text-xl mb-8 font-extrabold">
-							I worked at
-						</span>{" "}
-						<span className="inline-block">Lynk Law, Inc</span>
-						<span className="inline-block">
-							Tumbleweed Day Camp
-						</span>
-					</p>
-				</div>
-				<div className="mx-12 lg:mx-0 border-[1px] border-mmpurple p-8 flex justify-center lg:col-span-3">
-					<p className="text-white font-oswald text-4xl uppercase text-center px-8 py-8">
-						<span className="block font-robotoslab text-mmpink text-xl mb-8 font-extrabold">
-							My Roles Were
-						</span>{" "}
-						Client Manager; Assistant Director
-					</p>
-				</div>
+
+				{backgroundInfo.map((item, index) => (
+					<div
+						key={index}
+						className="mx-12 lg:mx-0 border-[1px] border-mmpurple p-8 flex justify-center lg:col-span-3"
+					>
+						<p className="text-white font-oswald text-4xl uppercase text-center px-8 py-8">
+							<span className="block font-robotoslab text-mmpink text-xl mb-8 font-extrabold">
+								{item.title}
+							</span>{" "}
+							{item.subtitle}
+						</p>
+					</div>
+				))}
 			</div>
 		</section>
   );
--- frontend/src/components/ShowcaseComponent.js
+++ frontend/src/components/ShowcaseComponent.js
@@ -1,69 +1,73 @@
 import React from "react";
 
 function ShowcaseComponent() {
+
+	const pageTitle = "Here's a showcase of my creative work"
+
+	  const showcaseImages = [
+			{
+				src: "/assets/images/image_mountains.jpg",
+				srcset: `/assets/images/image_mountains.jpg 500w,
+                /assets/images/image_mountains@2x.jpg 1000w,
+                /assets/images/image_mountains@3x.jpg 1500w`,
+				alt: "A geometric quilt with purple mountains.",
+			},
+			{
+				src: "/assets/images/image_night_lemur.jpg",
+				srcset: `/assets/images/image_night_lemur.jpg 500w,
+                /assets/images/image_night_lemur@2x.jpg 1000w,
+                /assets/images/image_night_lemur@3x.jpg 1500w`,
+				alt: "The night lemur, a foundation paper piece pillowtop.",
+			},
+			{
+				src: "/assets/images/image_rocket.jpg",
+				srcset: `/assets/images/image_rocket.jpg 500w,
+                /assets/images/image_rocket@2x.jpg 1000w,
+                /assets/images/image_rocket@3x.jpg 1500w`,
+				alt: "A cute rocketship baby quilt.",
+			},
+			{
+				src: "/assets/images/image_leopard.jpg",
+				srcset: `/assets/images/image_leopard.jpg 500w,
+                /assets/images/image_leopard@2x.jpg 1000w,
+                /assets/images/image_leopard@3x.jpg 1500w`,
+				alt: "A floral big cats leopard.",
+			},
+			{
+				src: "/assets/images/image_cactus.png",
+				srcset: `/assets/images/image_cactus.png 500w,
+                /assets/images/image_cactus@2x.png 1000w,
+                /assets/images/image_cactus@3x.png 1500w`,
+				alt: "A patterned cactus and flower quilt in terra cotta orange.",
+			},
+			{
+				src: "/assets/images/image_porthole_sewing.jpg",
+				srcset: `/assets/images/image_porthole_sewing.jpg 500w,
+                /assets/images/image_porthole_sewing@2x.jpg 1000w,
+                /assets/images/image_porthole_sewing@3x.jpg 1500w`,
+				alt: "A geometric quilt with purple mountains.",
+			},
+		];
+
 	return (
 		<section className="container max-w-7xl mx-auto bg-black w-full">
 			<div className="lg:grid lg:grid-flow-col lg:grid-cols-9 lg:mt-16 lg:mx-4 lg:justify-center ">
 				<div className="w-full mx-auto text-center lg:text-left lg:col-span-3 lg:flex lg:justify-center lg:items-center">
 					<h2 className="text-5xl text-mmpink uppercase py-12 md:pt-24 md:pb-12 mx-auto font-oswald lg:mx-8">
-						Here's a showcase of my creative work{" "}
+						{pageTitle}
 					</h2>
 				</div>
 				<div className="mx-8 grid grid-flow-col grid-rows-6 lg:grid-rows-2 lg:col-span-6 lg:grid-flow-row lg:grid-cols-3 gap-8 lg:gap-8">
-					<img
-						className="max-w-full h-auto object-cover"
-						src={"/assets/images/image_mountains.jpg"}
-						srcset="/assets/images/image_mountains.jpg 500w,
-                                    /assets/images/image_mountains@2x.jpg 1000w,
-                                    /assets/images/image_mountains@3x.jpg 1500w"
-						sizes="(max-width: 500px) 500px, (max-width: 1000px) 1000px, 1500px"
-						alt="A geometric quilt with purple mountains."
-					></img>
-					<img
-						className="max-w-full h-auto object-cover"
-						src={"/assets/images/image_night_lemur.jpg"}
-						srcset="/assets/images/image_ 500w,
-                                    /assets/images/image_night_lemur@2x.jpg 1000w,
-                                    /assets/images/image_night_lemur@3x.jpg 1500w"
-						sizes="(max-width: 500px) 500px, (max-width: 1000px) 1000px, 1500px"
-						alt="The night lemur, a foundation paperpiece pillowtop."
-					></img>
-					<img
-						className="max-w-full h-auto object-cover"
-						src={"/assets/images/image_rocket.jpg"}
-						srcset="/assets/images/image_rocket.jpg 500w,
-                                    /assets/images/image_rocket@2x.jpg 1000w,
-                                    /assets/images/image_rocket@3x.jpg 1500w"
-						sizes="(max-width: 500px) 500px, (max-width: 1000px) 1000px, 1500px"
-						alt="A cute rocketship baby quilt."
-					></img>
-					<img
-						className="max-w-full h-auto object-cover"
-						src={"/assets/images/image_leopard.jpg"}
-						srcset="/assets/images/image_leopard.jpg 500w,
-                                    /assets/images/image_leopard@2x.jpg 1000w,
-                                    /assets/images/image_leopard@3x.jpg 1500w"
-						sizes="(max-width: 500px) 500px, (max-width: 1000px) 1000px, 1500px"
-						alt="A floral big cats leopard."
-					></img>
-					<img
-						className="max-w-full h-auto object-cover"
-						src={"/assets/images/image_cactus.png"}
-						srcset="/assets/images/image_cactus.png 500w,
-                                    /assets/images/image_cactus@2x.png 1000w,
-                                    /assets/images/image_cactus@3x.png 1500w"
-						sizes="(max-width: 500px) 500px, (max-width: 1000px) 1000px, 1500px"
-						alt="A patterend cactus and flower quilt in terra cotta orange."
-					></img>
-					<img
-						className="max-w-full h-auto object-cover"
-						src={"/assets/images/image_porthole_sewing.jpg"}
-						srcset="/assets/images/image_porthole_sewing.jpg 500w,
-                                    /assets/images/image_porthole_sewing@2x.jpg 1000w,
-                                    /assets/images/image_porthole_sewing@3x.jpg 1500w"
-						sizes="(max-width: 500px) 500px, (max-width: 1000px) 1000px, 1500px"
-						alt="A geometric quilt with purple mountains."
-					></img>
+					{showcaseImages.map((image, index) => (
+						<img
+							key={index}
+							className="max-w-full h-auto object-cover"
+							src={image.src}
+							srcset={image.srcset}
+							sizes="(max-width: 500px) 500px, (max-width: 1000px) 1000px, 1500px"
+							alt={image.alt}
+						></img>
+					))}
 				</div>
 			</div>
 		</section>
